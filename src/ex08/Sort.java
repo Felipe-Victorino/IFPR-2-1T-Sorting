8244dfd3202beaebd7339a6d00418417afbbd98d
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-05-16 20:20:40 -0300

    Ex08 feito

--- src/ex08/Sort.java
+++ src/ex08/Sort.java
@@ -4,13 +4,13 @@ public class Sort {
 
     public static Paciente[] sortPaciente(Paciente[] lista){
         for (int c = 0; c < lista.length - 1; c++) {
-            //boolean swapped = false;
+            boolean swapped = false;
             for (int i = 0; i < lista.length - 1; i++) {
                 if (lista[i].getGravidade() < lista[i + 1].getGravidade()) {
                     Paciente aux = lista[i];
                     lista[i] = lista[i + 1];
                     lista[i + 1] = aux;
-                    //swapped = true;
+                    swapped = true;
                 }
                 if (lista[i].getGravidade() == lista[i + 1].getGravidade()) {
                     System.out.println("Gravidade igual");
@@ -18,12 +18,11 @@ public class Sort {
                         Paciente aux = lista[i];
                         lista[i] = lista[i + 1];
                         lista[i + 1] = aux;
+                        swapped = true;
                     }
-
-
                 }
             }
-            //if(!swapped){ break;}
+            if(!swapped){ break;}
         }
         return lista;
     }
