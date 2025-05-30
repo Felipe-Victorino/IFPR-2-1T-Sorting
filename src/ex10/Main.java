cd9288e50ed63aa70ab9852089346681dcdd167f
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-05-30 20:07:14 -0300

    Ex10 EVEN STILL kinda done?????

--- src/ex10/Main.java
+++ src/ex10/Main.java
@@ -6,22 +6,39 @@ public class Main {
     public static void main(String[] args) {
         int[] arrayRandom = new VectorGen().generateArray(5);
         int[] inverArray = new VectorGen().invertArray(arrayRandom);
+        int[] sortedArray = {1, 2, 3, 4, 5};
 
         System.out.println("BUBBLE SORT ===========");
+        System.out.println("Random Array: ");
         Main09.passToBubble(arrayRandom);
+        System.out.println("Inverted Array: ");
         Main09.passToBubble(inverArray);
+        System.out.println("Sorted Array: ");
+        Main09.passToBubble(sortedArray);
 
         System.out.println("INSERTION SORT ===========");
+        System.out.println("Random Array: ");
         Main09.passToInsertion(arrayRandom);
+        System.out.println("Inverted Array: ");
         Main09.passToInsertion(inverArray);
+        System.out.println("Sorted Array: ");
+        Main09.passToInsertion(sortedArray);
 
         System.out.println("SELECTION SORT ===========");
+        System.out.println("Random Array: ");
         Main09.passToSelection(arrayRandom);
+        System.out.println("Inverted Array: ");
         Main09.passToSelection(inverArray);
+        System.out.println("Sorted Array: ");
+        Main09.passToSelection(sortedArray);
 
         System.out.println("MERGE SORT ===========");
+        System.out.println("Random Array: ");
         Main09.passToMerge(arrayRandom);
+        System.out.println("Inverted Array: ");
         Main09.passToMerge(inverArray);
+        System.out.println("Sorted Array: ");
+        Main09.passToMerge(sortedArray);
 
     }
 }
